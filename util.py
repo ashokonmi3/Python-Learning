46cd1f0fce1e82c74db520faa3f62f9b31ee6582
Ashok Sharma <ashokonmi3@gmail.com>
2024-01-23 16:28:38 +0530

    added project

--- util.py
+++ util.py
@@ -1,5 +1,10 @@
-def get_path(filename):
-    """Return file's path or empty string if no path."""
-    import os
-    head, tail = os.path.split(filename)
-    return head
+# def get_path(filename):
+#     """Return file's path or empty string if no path."""
+#     import os
+#     head, tail = os.path.split(filename)
+#     return head
+import os
+import time
+print("hello")
+time.sleep(5)
+os.system('cls')
