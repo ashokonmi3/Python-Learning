c79296c2f8ed51b1d900798fa2c92a13b7a69d15
ashok <ashokonmi3@gmail.com>
2025-01-09 08:05:04 +0530

    changes for numpy

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\91973\Python_Training\myenv
+set VIRTUAL_ENV=D:\Training\Python_Learning\Python-Learning\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(myenv) 
 
 :END
 if defined _OLD_CODEPAGE (
